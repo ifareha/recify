fdb49865b559d62722c986a2980bf494e3ae2819
ifareha <khanfareha51@gmail.com>
2025-01-09 17:53:59 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://recify.onrender.com/', 
     credentials: true, 
 }));
 const _dirname = path.resolve();
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,2 @@
-export const USER_API_ = "http://localhost:8080/api/users"
-export const GET_ALL_RECIPE_API_ = "http://localhost:8080/api/recipes"
-// export const GET_ALL_RECIPE_API_ = "http://localhost:8080/api/recipes"
+export const USER_API_ = "https://recify.onrender.com/api/users"
+export const GET_ALL_RECIPE_API_ = "https://recify.onrender.com/api/recipes"
